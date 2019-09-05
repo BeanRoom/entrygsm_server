47b1a8435103f97a337a3ec8b84f4afcd847ea19
Hwarang Choi <az8606@naver.com>
2019-09-05 16:24:19 +0900

    User 모델 추가

---
+++ src/models/User.js
@@ -0,0 +1,47 @@
+export const User = (sequelize, DataTypes) => {
+    return sequelize.define('User', {
+        user_id : {
+            type : DataTypes.INTEGER,
+            primaryKey : true,
+            allowNull : false,
+            autoIncrement : true
+        },
+        id : {
+            type : DataTypes.STRING(50),
+            allowNull : false,
+            unique : true
+        },
+        password : {
+            type : DataTypes.STRING,
+            allowNull : false
+        },
+        name : {
+            type : DataTypes.STRING(50),
+            allowNull : false
+        },
+        school : {
+            type : DataTypes.STRING(10),
+            allowNull : false
+        },
+        grade : {
+            type : DataTypes.INTEGER,
+            allowNull : false
+        },
+        class : {
+            type : DataTypes.INTEGER,
+            allowNull : false
+        },
+        number : {
+            type : DataTypes.INTEGER,
+            allowNull : false
+        },
+        email : {
+            type : DataTypes.STRING,
+            allowNull : false
+        },
+        validation : {
+            type : DataTypes.BOOLEAN,
+            defaultValue : false
+        }
+    });
+};
