ce5279615e0fe5291d7ce7ee6fc26e88637cc4f5
Hwarang Choi <az8606@naver.com>
2019-09-20 02:31:36 +0900

    application 루트 설정

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 
 import auth from './auth';
+import application from './application';
 
 const api = new Router();
 
 api.use('/auth', auth.routes());
+api.use('/application', application.routes());
 
 export default api;
