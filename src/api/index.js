a7707b5dc3b195f87790e58095a1906bfa980a68
Hwarang Choi <az8606@naver.com>
2019-09-20 06:03:36 +0900

    QnA 기능 세팅

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,12 @@ import Router from 'koa-router';
 
 import auth from './auth';
 import application from './application';
+import qna from './qna';
 
 const api = new Router();
 
 api.use('/auth', auth.routes());
 api.use('/application', application.routes());
+api.use('/qna', qna.routes());
 
 export default api;
---
+++ src/api/qna/index.js
@@ -0,0 +1,8 @@
+import Router from 'koa-router';
+import {  } from './qna.controller';
+
+const qna = new Router();
+
+
+
+export default qna;
