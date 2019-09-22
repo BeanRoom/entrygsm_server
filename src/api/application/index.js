eb8d6812811087026d262820aaac164b5edad646
Hwarang Choi <az8606@naver.com>
2019-09-22 15:31:29 +0900

    api 수정

--- src/api/application/index.js
+++ src/api/application/index.js
@@ -1,12 +1,11 @@
 import Router from 'koa-router';
-import { SearchSchoolList, ApplicantInfo, ProtectorInfo, TeacherInfo, DeleteApplication } from './application.controller';
+import { SearchSchoolList, ApplicationInfo , DeleteApplication } from './application.controller';
 
 const application = new Router();
 
 application.get('/searchSchoolList', SearchSchoolList);
-application.post('/insert/applicant', ApplicantInfo);
-application.post('/insert/protector', ProtectorInfo);
-application.post('/insert/teacher', TeacherInfo);
+
+application.post('/application', ApplicationInfo);
 
 application.delete('/application', DeleteApplication);
 
